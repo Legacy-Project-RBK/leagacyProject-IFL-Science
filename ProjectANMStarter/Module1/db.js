75a277464975f995005d481da24753a5a1fb621d
mahanbahin <maha.a.nbahin@hotmail.com>
2019-11-18 20:25:28 +0200

    DB connection

--- ProjectANMStarter/Module1/db.js
+++ ProjectANMStarter/Module1/db.js
@@ -1,49 +1,48 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const URI = "mongodb+srv://Nour:nour@cluster0-x9aui.mongodb.net/science?retryWrites=true&w=majority"; //path?
-mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true}); //, useMongoClient: true
+const URI =
+  "mongodb+srv://maysqunaibi:123456789rbk@cluster0-mbxj6.mongodb.net/legacyContents?retryWrites=true&w=majority"; //path?
+
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }); //, useMongoClient: true
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
   // we're connected!
   console.log("We're Connected!");
 });
 
-
 const CommentSchema = Schema({
-  id_Content: {type: Number, unique: true}, //this is a foreign key for the article
-  text: {type: String},
-  likes: {type: Number}, 
-  date: {type: String}, 
-})
+  id_Content: { type: Number, unique: true }, //this is a foreign key for the article
+  text: { type: String },
+  likes: { type: Number },
+  date: { type: String }
+});
 
 const ContentSchema = Schema({
-  title: {type: String},
-  image:{type: String}, 
-  description: {type: String},
-  Auther: {type: String},
-  ImgAuther: {type: String},
-  date:{type:Date},
-  shares: {type: Number}
-})
+  title: { type: String },
+  image: { type: String },
+  description: { type: String },
+  Auther: { type: String },
+  ImgAuther: { type: String },
+  date: { type: Date },
+  shares: { type: Number }
+});
 
-const Comment = mongoose.model('Comment', CommentSchema);
-const Content = mongoose.model('Content', ContentSchema);
+const Comment = mongoose.model("Comment", CommentSchema);
+const Content = mongoose.model("Content", ContentSchema);
 
-let save = (article) => {  
-    var content = new Content({
-      title: article.title,
-      image:article.image, 
-      description: article.description,
-      Auther: article.Auther,
-      ImgAuther: article.ImgAuther,
-      date:article.date,
-      shares:article.shares
-    })
-    content.save();
-  }
+let save = article => {
+  var content = new Content({
+    title: article.title,
+    image: article.image,
+    description: article.description,
+    Auther: article.Auther,
+    ImgAuther: article.ImgAuther,
+    date: article.date,
+    shares: article.shares
+  });
+  content.save();
+};
 
-
-module.exports={Comment,save,Content};
-
+module.exports = { Comment, save, Content };
--- ProjectANMStarter/Module2/Database/index.js
+++ ProjectANMStarter/Module2/Database/index.js
@@ -1,38 +1,38 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const URI = "mongodb+srv://Nour:nour@cluster0-x9aui.mongodb.net/science?retryWrites=true&w=majority"
-mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true}); //, useMongoClient: true
+const URI =
+  "mongodb+srv://maysqunaibi:123456789rbk@cluster0-mbxj6.mongodb.net/legacyContents?retryWrites=true&w=majority"; //path?
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true }); //, useMongoClient: true
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
   console.log("We're Connected!");
 });
 
 const ContentSchema = Schema({
-  title: {type: String},
-  image:{type: String}, 
-  description: {type: String},
-  Auther: {type: String},
-  ImgAuther: {type: String},
-  date:{type:Date},
-  shares: {type: Number}
-})
+  title: { type: String },
+  image: { type: String },
+  description: { type: String },
+  Auther: { type: String },
+  ImgAuther: { type: String },
+  date: { type: Date },
+  shares: { type: Number }
+});
 
-const Content = mongoose.model('Content', ContentSchema);
+const Content = mongoose.model("Content", ContentSchema);
 
-
-let save = (article) => {  
+let save = article => {
   var content = new Content({
     title: article.title,
-    image:article.image, 
+    image: article.image,
     description: article.description,
     Auther: article.Auther,
     ImgAuther: article.ImgAuther,
-    date:article.date,
-    shares:article.shares
-  })
+    date: article.date,
+    shares: article.shares
+  });
   content.save();
-}
+};
 
-module.exports={save,Content};
+module.exports = { save, Content };
