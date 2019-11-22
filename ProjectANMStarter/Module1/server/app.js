db71e32c316b06f018854c57aeb8798a5f14d020
maysqunaibi <maysqunaibi2000@gmail.com>
2019-11-22 22:34:35 +0200

    integration testing

---
+++ ProjectANMStarter/Module1/server/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+//var ObjectId = require('mongodb').ObjectId;
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const db = require("../db.js");
+let app = express();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + "/../client/src/dist"));
+const Content = require("../db.js").Content;
+
+app.post("/shares", function(req, res) {
+  console.log("sucees post increaments shares");
+  const id = req.body.user_id;
+  // db.Content.update(  { _id:id} , { $set: { 'shares' : shares + 1  } } );
+  db.Content.update({ _id: id }, { $inc: { shares: 1 } });
+});
+
+app.get("/content", function(req, res) {
+  console.log(req.query, "reeeeeqqqqq");
+  var n = req.query._id;
+  console.log("get data from server");
+  Content.findOne({ _id: n }).exec((err, content) => {
+    if (err) {
+      console.log(err);
+      //req.send();
+    }
+    //console.log(content)
+    res.json(content);
+  });
+});
+
+app.get("/save", (req, res) => {
+  db.save();
+  res.send("saveed");
+});
+
+app.get("*", (req, res) => {
+  app.use(express.static(__dirname + "/../client/src/dist"));
+});
+
+module.exports = app;
